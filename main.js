a7cc41de27f32cdd4b588460de3cda4ec3289d31
Luciano <lucianortiz97@gmail.com>
2022-04-13 23:12:20 -0300

    apuntar a mouse y algunos refactors

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//VER DE USAR GLMATRIX PARA EL RAYCAST? (si es posible)
+
 const M_PI_180 = Math.PI/180
 const MAX_DEPTH=10;
 
@@ -6,7 +8,7 @@ const xsize = 6;
 const ysize = 6;
 
 
-const grid_size=128;
+const grid_size=200;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -17,52 +19,55 @@ var playery = 2;
 
 var direction = 50; //grados
 
-const MAX_REBOTES = 500;
+const MAX_REBOTES = 30;
 
 
-window.onload = drawRay()
+window.onload = main()
 
 window.addEventListener('keydown', onKeyboardPress);
+
+window.addEventListener('mousemove', mouse_monitor)
+
+
+var prev_mousex = 0;
+var prev_mousey = 0;
+function mouse_monitor(e) {
+    var mouse_x = e.pageX;
+    var mouse_y = e.pageY;
+
+    direction = Math.atan2(mouse_y - (playery-1)*(grid_size), mouse_x-(playerx-1)*(grid_size))*180/Math.PI
+    
+    if (direction >= Math.PI*2) {
+      direction -= Math.PI*2;
+    } else if (direction < 0) {
+        direction +=360;
+    }
+
+    main()
+  }
 
 function onKeyboardPress(event) {
     var code = event.code;
     // Alert the key name and key code on keydown
     //console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
     switch(code){
-        case 'KeyW':
-            direction-=1;
-            drawRay()
-            break;
         case 'KeyS':
-            direction+=1;
+            playery+=0.08;
             drawRay()
             break;
-
-        case 'ArrowDown':
-            playery+=0.03;
+        case 'KeyW':
+            playery-=0.08;
             drawRay()
             break;
-        case 'ArrowUp':
-            playery-=0.03;
-            drawRay()
-            break;
-
-        case 'ArrowLeft':
-          playerx-=0.03;
+        case 'KeyA':
+          playerx-=0.08;
           drawRay()
           break;
-
-        case 'ArrowRight':
-          playerx+=0.03;
+        case 'KeyD':
+          playerx+=0.08;
           drawRay()
           break;
     }
-    if (direction >= 360) {
-      direction -= 360;
-   } else if (direction < 0) {
-      direction +=360;
-   }
-
 }
 
 
@@ -74,75 +79,69 @@ function reflect(dir_x,dir_y,normal_x,normal_y){
   return [ref_x,ref_y]
 }
 
-function onKeyboardUp(event) {
-    var code = event.code;
-    //console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
-   dict_keys[code] = false;
+
+function main(){
+  var canvas = document.getElementById('canvas');
+  var myCtx = canvas.getContext('2d'); // devuelve un CanvasRenderingContext2D
+  myCtx.fillStyle = "black"
+  myCtx.fillRect(0,0,canvas.width,canvas.height)
+
+  myCtx.lineWidth = 2;
+
+  drawMap(myCtx,myMap);
+
+  var raystart_x = playerx;
+  var raystart_y = playery;
+  var ref_dir = direction
+  for(var i = 0; (i < MAX_REBOTES); i++){
+
+      //dibujar rayo en direccion
+      //  ctx.strokeStyle = `rgba(128,128,128,0.5)`;
+      //  ctx.beginPath();
+      //  ctx.moveTo(raystart_x*grid_size,raystart_y*grid_size);
+      //  ctx.lineTo(grid_size*(raystart_x+10*Math.cos(ref_dir*Math.PI/180)),grid_size*(raystart_y+10*Math.sin(ref_dir*Math.PI/180)));
+      //  ctx.stroke();
+      //
+      
+      var [raycolision_x,raycolision_y,wallNormal_x, wallNormal_y] = castRay(raystart_x,raystart_y,ref_dir);
+      
+      // dibujar flecha con normal del objeto colisionado
+      // canvas_arrow(ctx, raycolision_x * grid_size,raycolision_y* grid_size,(raycolision_x+wallNormal_x)* grid_size, (raycolision_y+wallNormal_y)* grid_size)
+      // ctx.stroke()
+      drawRay(myCtx,raystart_x, raystart_y, raycolision_x,raycolision_y, `rgba(0,255,0, ${(MAX_REBOTES-i)/MAX_REBOTES})`)
+
+      var raystart_x = raycolision_x;
+      var raystart_y = raycolision_y;
+
+      var [vecreflect_x, vecreflect_y]  = reflect(Math.cos(ref_dir*M_PI_180),Math.sin(ref_dir*M_PI_180),wallNormal_x,wallNormal_y)
+      ref_dir = Math.atan2(vecreflect_y,vecreflect_x)*180/Math.PI;
+
+      if (ref_dir >= 360) {
+        ref_dir -= 360;
+     } else if (ref_dir < 0) {
+      ref_dir +=360;
+     }
+  }
+
 }
 
- function drawRay(){
-
-    var canvas = document.getElementById('canvas');
-    
-    var ctx = canvas.getContext('2d'); // devuelve un CanvasRenderingContext2D
-
-    ctx.fillStyle = "black"
-    ctx.fillRect(0,0,canvas.width,canvas.height)
-    
-    ctx.lineWidth = 2;
-  
-    // dibujo paredes//
-    ctx.fillStyle = `rgba(255,0,0,0.5)`
-    for (var i = 0; i < xsize; i++){
-      for (var j = 0; j < ysize; j++){
-        if (myMap[i][j]==1){
-          ctx.fillRect(i*grid_size,j*grid_size,grid_size,grid_size);
-        }
+function drawMap(ctx,map){
+  ctx.fillStyle = `rgba(255,0,0,0.5)`
+  for (var i = 0; i < xsize; i++){
+    for (var j = 0; j < ysize; j++){
+      if (map[i][j]==1){
+        ctx.fillRect(i*grid_size - grid_size,j*grid_size- grid_size,grid_size,grid_size);
       }
     }
-    //
+  }
+}
 
-
-    var raystart_x = playerx;
-    var raystart_y = playery;
-    var ref_dir = direction
-    for(var i = 0; (i < MAX_REBOTES); i++){
-
-        //dibujar rayo en direccion
-        //  ctx.strokeStyle = `rgba(128,128,128,0.5)`;
-        //  ctx.beginPath();
-        //  ctx.moveTo(raystart_x*grid_size,raystart_y*grid_size);
-        //  ctx.lineTo(grid_size*(raystart_x+10*Math.cos(ref_dir*Math.PI/180)),grid_size*(raystart_y+10*Math.sin(ref_dir*Math.PI/180)));
-        //  ctx.stroke();
-        //
-
-        
-        
-        var [raycolision_x,raycolision_y,wallNormal_x, wallNormal_y] = castRay(raystart_x,raystart_y,ref_dir);
-        
-        // dibujar flecha con normal del objeto colisionado
-        // canvas_arrow(ctx, raycolision_x * grid_size,raycolision_y* grid_size,(raycolision_x+wallNormal_x)* grid_size, (raycolision_y+wallNormal_y)* grid_size)
-        // ctx.stroke()
-
-        ctx.strokeStyle = `rgba(0,255,0, ${(MAX_REBOTES-i)/MAX_REBOTES})`; //`rgb(128,128,255)`;//
-        ctx.beginPath();
-        ctx.moveTo(raystart_x*grid_size,raystart_y*grid_size);
-        ctx.lineTo(raycolision_x*grid_size,raycolision_y*grid_size);
-        ctx.stroke();
-
-        var raystart_x = raycolision_x;
-        var raystart_y = raycolision_y;
-
-        var [vecreflect_x, vecreflect_y]  = reflect(Math.cos(ref_dir*M_PI_180),Math.sin(ref_dir*M_PI_180),wallNormal_x,wallNormal_y)
-        ref_dir = Math.atan2(vecreflect_y,vecreflect_x)*180/Math.PI;
-
-        if (ref_dir >= 360) {
-          ref_dir -= 360;
-       } else if (ref_dir < 0) {
-        ref_dir +=360;
-       }
-        //await sleep(1000);    
-    }
+ function drawRay(ctx,start_x,start_y,end_x,end_y,color){
+    ctx.strokeStyle = color; 
+    ctx.beginPath();
+    ctx.moveTo(start_x*grid_size - grid_size ,start_y*grid_size - grid_size);
+    ctx.lineTo(end_x*grid_size - grid_size ,end_y*grid_size - grid_size);
+    ctx.stroke();
 }
 
 function getTileSteps(dir) {
@@ -163,21 +162,21 @@ function getTileSteps(dir) {
   }
 
 
-function getXYSteps(direction) {
-    if (direction == 360) {
+function getXYSteps(dir) {
+    if (dir == 360) {
       xStep = 60;
       yStep = 0;
-    } else if (direction == 180) {
+    } else if (dir == 180) {
       xStep = -60;
       yStep = 0;
-    } else if (direction == 90) {
+    } else if (dir == 90) {
       xStep = 0;
       yStep = -60;
-    } else if (direction == 270) {
+    } else if (dir == 270) {
       xStep = 0;
       yStep = 60;
     } else {
-      auxTanDirection = Math.tan(direction * M_PI_180);
+      auxTanDirection = Math.tan(dir * M_PI_180);
       xStep = tileStepY/auxTanDirection;
       yStep = tileStepX*auxTanDirection;
     }
@@ -194,7 +193,7 @@ function getXYSteps(direction) {
     return myMap[y][x];
   }
 
-  function castRay(raystart_x, raystart_y, direction) {
+  function castRay(raystart_x, raystart_y, dir) {
 
     tileX = Math.floor(raystart_x);
     dx = raystart_x - tileX;
@@ -204,38 +203,31 @@ function getXYSteps(direction) {
     x = tileX;
     y = tileY;
  
-     var [tileStepX, tileStepY] = getTileSteps(direction);
+     var [tileStepX, tileStepY] = getTileSteps(dir);
  
-     var [xStep, yStep] = getXYSteps(direction);
+     var [xStep, yStep] = getXYSteps(dir);
  
      var xIntercept, yIntercept;
  
-     if (direction <= 180) {
+     if (dir <= 180) {
        xIntercept = x + dx + (1-dy)*xStep;
      } else {
        xIntercept = x + dx + dy*xStep;
      }
  
-     if (direction >= 90 && direction <= 270) {
+     if (dir >= 90 && dir <= 270) {
        yIntercept = y + dy + (dx)*yStep;
      } else {
        yIntercept = y + dy + (1-dx)*yStep;
      }
 
-
-
-
-    
- 
      // Loopea hasta encontrar pared tanto horizontal como vertical (o pasar max_depth)
      wallFoundX = null;
      wallFoundY = null;
      
-
-      //
       var canvas = document.getElementById('canvas');
-    
-      var ctx = canvas.getContext('2d'); // devuelve un CanvasRenderingContext2D
+  
+     var ctx = canvas.getContext('2d'); // devuelve un CanvasRenderingContext2D
 
       //
      for (i = 0; i <= MAX_DEPTH && !(wallFoundX && wallFoundY) ; i++) {
@@ -284,7 +276,6 @@ function getXYSteps(direction) {
     // ctx.stroke();
     //
 
-    console.log(`green d: ${d1}, blue d: ${d2}`)
     var wallNormalX , wallNormalY;
      if (d1 < d2) {
          var [colisionx, colisiony] = [colhx, colhy];
