80b1eb27221f3bdc81a8a3a1a24e0d321a64c06c
luctiz <lucianortiz97@gmail.com>
2024-04-27 16:42:02 -0300

    remove comments

--- modules/node.js
+++ modules/node.js
@@ -5,34 +5,22 @@ export default class Node {
     this.isAlive = true;
   }
 
-  update(){
+  update() {
     for (let i = this.childNodes.length - 1; i >= 0; i--) {
-        const item = this.childNodes[i];
-    
-        if (item.isAlive) {
-            // Call the update method if the item is alive
-            item.update();
-        } else {
-            // Remove the item from the list if it's not alive
-            this.childNodes.splice(i, 1);
-        }
+      const item = this.childNodes[i];
+      if (item.isAlive) {
+        item.update();
+      } else {
+        this.childNodes.splice(i, 1);
+      }
     }
   }
 
   render() {
-    this.childNodes.filter(x => x.isAlive).forEach((c) => c.render()); 
-    // capaz falla el splice?
-    // el filter por isAlive no debería necesitarlo :/
+    this.childNodes.filter((x) => x.isAlive).forEach((c) => c.render());
   }
 
   kill() {
     this.isAlive = false;
   }
-
-
-  // player crea un rayo por frame
-  // rayo se crea en un update con un life de 1.
-  // rayo se dibuja en ese frame
-  // en el proximo update, el rayo se va a marcar con life de 0
-  // el update lo puedo hacer desde abajo hacia arriba, entonces el update del padre va a borrarlo de la lista, y luego no se va a ver el rayo?
 }
